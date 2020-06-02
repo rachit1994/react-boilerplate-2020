2adefed1063afd3bbbb7a868ef424a365694f8e5
rachit-aasaanjobs <rachit.srivastava@aasaanjobs.com>
2020-06-02 13:36:02 +0530

    setup complete

--- src/utils/graphql/client.ts
+++ src/utils/graphql/client.ts
@@ -6,10 +6,9 @@ const cookieObj = getUserFromCookies() || {
 };
 console.log('coming in cookies', cookieObj);
 export const apolloClient = new ApolloClient({
-    uri: 'https://graphql2.aasaanjobs.net/',
+    uri: '',
     headers: {
-        Authorization: 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX3R5cGUiOiJQQSIsImVudGl0eV9pZCI6IjMzNDYiLCJuYW1lIjoiUmFjaGl0IFNyaXZhc3RhdmEiLCJpZCI6IjEwMzBiZWQyLWE0MzItNDg4My1iZjJlLWIzMjRiOGRjMWE1YiJ9.cQrWzXKuhnnDa7sl0PDcaCTyGvXivmSQABQt4sLVMC4',
-        'x-aj-platform': 1,
+        Authorization: '',
     },
 });
 
